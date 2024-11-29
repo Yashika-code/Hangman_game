e1054e9f4ce774b5b1c075092c5c27debe62c806
Yashika soni <soniyashika164@gmail.com>
2024-11-29 19:32:54 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,39 @@
+import  random
+name=input("enter your name : ")
+print("Good Luck!!",name)
+
+words=['rainbow','computer','science','python','board','player','reverse','football','cricket','water','condition']
+
+word=random.choice(words)
+
+print("Guess the characters : ")
+guesses=''
+turns=12
+
+while turns>0:
+    failed=0
+
+    for char in word:
+        if char in guesses:
+            print(char,end=" ")
+        else:
+            print("_")
+            failed += 1
+
+    if failed == 0:
+        print("you win")
+        print("The word is : ",word)
+        break
+
+    print()
+    guess=input("guess a character  : ")
+    guesses += guess
+
+    if guess not in word:
+
+        turns -= 1
+        print("Wrong")
+        print("You have",+ turns,' more guesses')
+
+        if turns==0:
+            print("You loose")
